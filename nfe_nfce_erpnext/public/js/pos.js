36d872ee96b7405c7a9930e3a4b309d5cfb94d2f
Shirkit <Shirkit@users.noreply.github.com>
2024-11-22 10:35:14 -0300

    feat: :sparkles: Better image abbreviation
    Instead of just grabbing some characters for the image placeholder, it can now parse the item description there's an 'ABBR:xxxx'  present, or, if not, it grabs the more logical deduced name for plants.

--- nfe_nfce_erpnext/public/js/pos.js
+++ nfe_nfce_erpnext/public/js/pos.js
@@ -57,6 +57,128 @@ frappe.require('point-of-sale.bundle.js', function () {
         }
     }
 
+    erpnext.PointOfSale.ItemSelector = class MyPosSelector extends erpnext.PointOfSale.ItemSelector {
+        constructor({ frm, wrapper, events, pos_profile, settings }) {
+            super({ frm, wrapper, events, pos_profile, settings })
+        }
+
+        get_item_html(item) {
+            const me = this;
+            // eslint-disable-next-line no-unused-vars
+            const { item_image, serial_no, batch_no, barcode, actual_qty, uom, price_list_rate, description } = item;
+            const precision = flt(price_list_rate, 2) % 1 != 0 ? 2 : 0;
+            let indicator_color;
+            let qty_to_display = actual_qty;
+
+            if (item.is_stock_item) {
+                indicator_color = actual_qty > 10 ? "green" : actual_qty <= 0 ? "red" : "orange";
+
+                if (Math.round(qty_to_display) > 999) {
+                    qty_to_display = Math.round(qty_to_display) / 1000;
+                    qty_to_display = qty_to_display.toFixed(1) + "K";
+                }
+            } else {
+                indicator_color = "";
+                qty_to_display = "";
+            }
+
+            function get_item_image_html() {
+                if (!me.hide_images && item_image) {
+                    return `<div class="item-qty-pill">
+                                <span class="indicator-pill whitespace-nowrap ${indicator_color}">${qty_to_display}</span>
+                            </div>
+                            <div class="flex items-center justify-center h-32 border-b-grey text-6xl text-grey-100">
+                                <img
+                                    onerror="cur_pos.item_selector.handle_broken_image(this)"
+                                    class="h-full item-img" src="${item_image}"
+                                    alt="${frappe.abbr(item.item_name)}"
+                                >
+                            </div>`;
+                } else {
+                    if (me.hide_images) return ""
+                    return `<div class="item-qty-pill">
+                                <span class="indicator-pill whitespace-nowrap ${indicator_color}">${qty_to_display}</span>
+                            </div>
+                            <div class="item-display abbr">${abbr(item.item_name, description)}</div>`;
+                }
+            }
+
+            function abbr(txt, desc) {
+                console.log(txt, desc)
+                if (!txt) return "";
+                if (desc) {
+                    const split = strip(desc).split(":")
+                    if (split.length > 1) {
+                        if (split[0].slice(-4) == "ABBR") {
+                            return split[1].trim();
+                        }
+                    }
+                }
+                var abbr = "";
+                $.each(txt.split("-"), function (i, w) {
+                    if (abbr.length == 0) {
+                        if (w.startsWith("Phal")) abbr = "Phal"
+                        else if (w.startsWith("Dend")) abbr = "Dend"
+                        else {
+                            abbr = w.substring(0, 3) + " "
+                        }
+                    } else {
+                        if (w == "Tronco")
+                            abbr += " Tronco";
+                        else {
+                            abbr += " " + w.trim()[0];
+                            if (isNumber(w.slice(-1))) {
+                                if (isNumber(w.slice(-2))) {
+                                    abbr += w.slice(-2);
+                                } else {
+                                    abbr += w.slice(-1);
+                                }
+                            }
+                        }
+                    }
+                });
+
+                return abbr || "?";
+            };
+
+            function strip(html){
+                let doc = new DOMParser().parseFromString(html, 'text/html');
+                return doc.body.textContent || "";
+             }
+
+
+            function isNumber(num) {
+                switch (typeof num) {
+                    case 'number':
+                        return num - num === 0;
+                    case 'string':
+                        if (num.trim() !== '')
+                            return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
+                        return false;
+                    default:
+                        return false;
+                }
+            }
+
+            return `<div class="item-wrapper"
+                    data-item-code="${escape(item.item_code)}" data-serial-no="${escape(serial_no)}"
+                    data-batch-no="${escape(batch_no)}" data-uom="${escape(uom)}"
+                    data-rate="${escape(price_list_rate || 0)}"
+                    title="${item.item_name}">
+    
+                    ${get_item_image_html()}
+    
+                    <div class="item-detail">
+                        <div class="item-name">
+                            ${frappe.ellipsis(item.item_name, 38)}
+                        </div>
+                        <div class="item-rate">${format_currency(price_list_rate, item.currency, precision) || 0} / ${uom == "Unidade" ? "un" : uom}</div>
+                    </div>
+                </div>`;
+        }
+
+    }
+
     erpnext.PointOfSale.PastOrderSummary = class MyPastOrderSummary extends erpnext.PointOfSale.PastOrderSummary {
         constructor({ wrapper, events }) {
             events.imprimir_nf = (name) => {
