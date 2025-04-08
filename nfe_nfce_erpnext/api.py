cec0ddd1b0a5bcf8e3837a5aebe3e92edac63d3c
Shirkit <shirkit@gmail.com>
2025-04-08 19:43:27 +0000

    Local modifications

--- nfe_nfce_erpnext/api.py
+++ nfe_nfce_erpnext/api.py
@@ -566,12 +566,12 @@ def criarNotaFiscal(*args, **kwargs):
         nota.produtos.append(produto)
 
     x = 0
-    nota.valor_pagamento = 0
+    nota.valor_pagamento = None
+    nota.forma_pagamento = None
     sem_pagamento = selectOption(
         str(90),
         frappe.get_meta("Nota Fiscal").get_field("forma_pagamento").options.split("\n"),
     )
-    nota.forma_pagamento = sem_pagamento
     for payment in server_doc.payments:
         mode_of_payment = frappe.get_doc(
             "Mode of Payment",
@@ -633,12 +633,12 @@ def criarNotaFiscal(*args, **kwargs):
             .options.split("\n"),
         )
 
-    if insert and server_doc is not None and nota.valor_pagamento > 0 and nota.forma_pagamento != sem_pagamento:
+    if insert and server_doc is not None and nota.forma_pagamento != sem_pagamento:
         # print("Inserting")
         nota.insert()
         nota.save()
-        frappe.db.commit()
         server_doc.db_set("nf_ultima_nota", nota.name, notify=True, commit=True)
+        frappe.db.commit()
         #server_doc.save()
 
         if submit:
